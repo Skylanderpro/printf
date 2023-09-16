20a1377fd5cc90ea6e8e50126f5601d619ebda8f
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-16 05:14:36 -0700

    Applied modifications to files: _printf.c

--- _printf.c
+++ _printf.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
  * _printf - Function to produce output according to a format
  * @format: constant char
  * Return: No of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list vargs;
 	int v, y, count = 0;
-	char *s;
-	char p;
+	char *s, p;
 
 	y = strlen(format);
 	va_start(vargs, format);
-
 	for (v = 0; v < y; v++)
 	{
 		if (format[v] == '%')
@@ -32,7 +28,7 @@ int _printf(const char *format, ...)
 				}
 			}
 			if (format[v] == 'c')
-			{	
+			{
 				p = va_arg(vargs, int);
 				putchar(p);
 				count++;
