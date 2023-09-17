78c76639a552da599f9c8e76ffd5f99e86f18831
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-17 21:46:07 +0300

    Applied modifications to files: _printf.c main.h

--- _printf.c
+++ _printf.c
@@ -1,104 +1,16 @@
 #include "main.h"
 
 /**
- * print_char - Print a single character and return the character count.
- * @c: Character to be printed.
- *
- * Return: Number of characters printed (always 1 for a single character).
- */
-int print_char(char c)
-{
-	putcher(c);
-	return (1);
-}
-
-/**
- * print_string - Print a string and return the character count.
- * @str: String to be printed.
- *
- * Return: Number of characters printed.
- */
-int print_string(const char *str)
-{
-	int count = 0;
-
-	while (*str)
-	{
-		if (str == NULL || *str == '\0')
-		{
-			str = "";
-		}
-		putcher(*str);
-		str++;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * print_integer - Print an integer and return the character count.
- * @n: Integer to be printed.
- *
- * Return: Number of characters printed.
- */
-int print_integer(int n)
-{
-	int count = 0, digit, i, num = n, divisor, temp, num_digits = 1;
-
-	if (n < 0)
-	{
-		putcher('-');
-		num = -num;
-		count++;
-	}
-
-
-	temp = num;
-	while (temp > 9)
-	{
-		temp /= 10;
-		num_digits++;
-	}
-
-	divisor = 1;
-
-	for (i = 1; i < num_digits; i++)
-		divisor *= 10;
-
-	count++;
-	while (divisor > 0)
-	{
-		digit = num / divisor;
-		putcher('0' + digit);
-		count++;
-		num -= digit * divisor;
-		divisor /= 10;
-	}
-
-	return (count);
-}
-
-/**
- *put_perc - prints percent
- *Return: number of printed characters
- */
-int put_perc(void)
-{
-	putcher('%');
-	return (1);
-}
-
-/**
- * _printf - Custom printf function, handle %c,%s,%d,%i,and %% specifiers.
- * @format: Format string containing specifiers.
- *
- * Return: Total number of characters printed.
+ *_printf - custom printf function
+ *@format: string argument to printf
+ *Return: count
  */
 int _printf(const char *format, ...)
 {
 	va_list vargs;
 	const char *ptr;
-	int count = 0;
+	int count = 0, buffer_index = 0;
+	char buffer[BUFFER_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -111,29 +23,25 @@ int _printf(const char *format, ...)
 		{
 			ptr++;
 			if (*ptr == 'c')
-			{
-				count += print_char((char)va_arg(vargs, int));
-			}
+				print_c((char)va_arg(vargs, int));
 			else if (*ptr == 's')
-			{
-				count += print_string(va_arg(vargs, char *));
-			}
+				print_s(va_arg(vargs, char *));
 			else if (*ptr == 'd' || *ptr == 'i')
-			{
-				count += print_integer(va_arg(vargs, int));
-			}
+				print_di(va_arg(vargs, int));
 			else if (*ptr == '%')
-			{
-				count += put_perc();
-			}
+				print_perc();
 		}
 		else
 		{
-			putcher(*ptr);
-			count++;
+			if (buffer_index >= BUFFER_SIZE - 1)
+				print_buffer(buffer, &count, &buffer_index);
+			buffer[buffer_index++] = *ptr;
 		}
 	}
 
+	if (buffer_index > 0)
+		print_buffer(buffer, &count, &buffer_index);
+
 	va_end(vargs);
-	return (count);
+	return count;
 }
