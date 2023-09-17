f841e5becde922696703648d33fcec0ae9dac5e9
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-17 23:10:42 +0300

    Applied modifications to files: print.c putcher.c

--- print.c
+++ print.c
@@ -18,13 +18,13 @@ void print_buffer(char *buffer, int *count, int *buffer_index)
  *@count: nu of printed chars
  *buffer_index: pointer to an int
  */
-void print_c(char c, char *buffer, int *count, int buffer_index)
+void print_c(char c, char *buffer, int *count, int *buffer_index)
 {
-	if (buffer_index >= BUFFER_SIZE - 1)
+	if (*buffer_index >= BUFFER_SIZE - 1)
 	{
-		print_buffer(buffer, &count, &buffer_index);
+		print_buffer(buffer, count, buffer_index);
 	}
-	buffer[buffer_index++] = c;
+	buffer[*buffer_index++] = c;
 }
 /**
  *print_s - adds a string to buffer
@@ -42,12 +42,12 @@ void print_s(const char *str, char *buffer, int *count, int *buffer_index)
 		str = "(null)";
 	}
 	len = strlen(str);
-	if (buffer_index + len >= BUFFER_SIZE - 1)
+	if (*buffer_index + len >= BUFFER_SIZE - 1)
 	{
-		print_buffer(buffer, &count, &buffer_index);
+		print_buffer(buffer, count, buffer_index);
 	}
-	strncpy(buffer + buffer_index, str, len);
-	buffer_index += len;
+	strncpy(buffer + *buffer_index, str, len);
+	*buffer_index += len;
 }
 /**
  *print_di - adds %d and %i arguments of printf to buffer
@@ -62,12 +62,12 @@ void print_di(int num, char *buffer, int *count, int *buffer_index)
 	char num_str[20];
 	snprintf(num_str, sizeof(num_str), "%d", num);
 	len = strlen(num_str);
-	if (buffer_index + len >= BUFFER_SIZE - 1)
+	if (*buffer_index + len >= BUFFER_SIZE - 1)
 	{
-		print_buffer(buffer, &count, &buffer_index);
+		print_buffer(buffer, count, buffer_index);
 	}
-	strncpy(buffer + buffer_index, num_str, len);
-	buffer_index += len;
+	strncpy(buffer + *buffer_index, num_str, len);
+	*buffer_index += len;
 }
 /**
  *print_perc - adds percent to buffer
@@ -77,7 +77,7 @@ void print_di(int num, char *buffer, int *count, int *buffer_index)
  */
 void print_perc(char *buffer, int *count, int *buffer_index)
 {
-	if (buffer_index >= BUFFER_SIZE - 1)
-		print_buffer(buffer, &count, &buffer_index);
-	buffer[buffer_index++] = '%';
+	if (*buffer_index >= BUFFER_SIZE - 1)
+		print_buffer(buffer, count, buffer_index);
+	buffer[*buffer_index++] = '%';
 }
