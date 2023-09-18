a24bbb54f31b06475792523ed64ff32ae8a18fb8
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-18 12:23:30 -0700

    Applied modifications to files: _printf.c _printf_2.c handle_specifier.c

--- _printf.c
+++ _printf.c
@@ -20,29 +20,13 @@ int _printf(const char *format, ...)
 	{
 		if (*ptr == '%')
 		{
-			ptr++;
-			if (*ptr == 'c')
-				print_c((char)va_arg(vargs, int), buffer, &count,
-						&buffer_index);
-			else if (*ptr == 's')
-				print_s(va_arg(vargs, char *), buffer, &count,
-						&buffer_index);
-			else if (*ptr == 'd' || *ptr == 'i')
-				print_di(va_arg(vargs, int), buffer, &count,
-						&buffer_index);
-			else if (*ptr == '%')
-				print_perc(buffer, &count, &buffer_index);
-			else if (*ptr == 'b')
-			{
-				num = va_arg(vargs, unsigned int);
-				print_binary(num, buffer, &count, &buffer_index);
-			}
-			else
-			{
-				if (buffer_index >= BUFFER_SIZE - 1)
-					print_buffer(buffer, &count, &buffer_index);
-				buffer[buffer_index++] = *ptr;
-			}
+			handle_format(&ptr, vargs, buffer, &count, &buffer_index, &num);
+		}
+		else
+		{
+			if (buffer_index >= BUFFER_SIZE - 1)
+				print_buffer(buffer, &count, &buffer_index);
+			buffer[buffer_index++] = *ptr;
 		}
 	}
 	if (buffer_index > 0)
