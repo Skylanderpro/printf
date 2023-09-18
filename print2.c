31d7e406fbb955cc2eaa9b20ef4cda9417a3991e
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-18 08:03:21 -0700

    Applied modifications to files: main.h print2.c

--- print2.c
+++ print2.c
@@ -49,36 +49,97 @@ void print_binary(unsigned int num, char *buffer, int *count,
 	}
 }
 
-void print_unsigned(va_list args, char *buffer, int *count, int *buffer_index)
+/**
+ * print_unsigned - Function to print unsigned values
+ *@num: unsigned int
+ *@buffer: stores chars to print
+ *@count: No of printed char
+ *@buffer_index: chars in buffer
+ */
+
+void print_unsigned(unsigned int num, char *buffer, int *count, int *buffer_index)
 {
-	unsigned int num = va_arg(args, unsigned int);
 	char num_str[20];
 	int len, b;
 
 	snprintf(num_str, sizeof(num_str), "%u", num);
 	len = strlen(num_str);
 
-	for (b = 0; b <len; b++)
+	for (b = 0; b < len; b++)
 	{
 		if (*buffer_index >= BUFFER_SIZE - 1)
 			print_buffer(buffer, count, buffer_index);
-		buffer[(*buffer_index)++] = num_str[i];
+		buffer[(*buffer_index)++] = num_str[b];
 	}
 }
 
-void print_octal(va_list args, char *buffer, int *count, int *buffer_index)
+/**
+ * print_octal - Function to print octal nos
+ * @num: unsigned int
+ * @buffer: stores chars to be printed
+ * @count: no of chars printed
+ * @buffer_index: chars in buffer
+ */
+void print_octal(unsigned int num, char *buffer, int *count, int *buffer_index)
 {
-	unsigned int num = va_arg(args, unsigned int);
 	char octal_str[20];
 	int len, i;
 
 	snprintf(octal_str, sizeof(octal_str), "%o", num);
 	len = strlen(octal_str);
 
-	for (int i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (*buffer_index >= BUFFER_SIZE - 1)
 			print_buffer(buffer, count, buffer_index);
 		buffer[(*buffer_index)++] = octal_str[i];
+	}
+}
+
+/**
+ * print_hexad_lower - print hexadecimal values in small letters
+ * @num: unsigned int
+ * @buffer: storeschars to be printed
+ * @count: no of chars printed
+ * @buffer_index: chars in buffer
+ */
+void print_hexad_lower(unsigned int num, char *buffer, int *count,
+		int *buffer_index)
+{
+	char hex_str[20];
+	int len, l;
+
+	snprintf(hex_str, sizeof(hex_str), "%x", num);
+	len = strlen(hex_str);
+
+	for (l = 0; l < len; l++)
+	{
+		if (*buffer_index >= BUFFER_SIZE - 1)
+			print_buffer(buffer, count, buffer_index);
+		buffer[(*buffer_index)++] = hex_str[l];
+	}
+}
+
+/**
+ * print_hexad_upper - Function to printhexadecimal in caps
+ * @num: unsigned int
+ * @buffer: stores chars to print
+ * @count: no of chars printed
+ * @buffer_index: chars in buffer
+ */
+void print_hexad_upper(unsigned int num, char *buffer, int *count,
+		int *buffer_index)
+{
+	char hex_str[20];
+	int len, u;
+
+	snprintf(hex_str, sizeof(hex_str), "%X", num);
+	len = strlen(hex_str);
+
+	for (u = 0; u < len; u++)
+	{
+		if (*buffer_index >= BUFFER_SIZE - 1)
+			print_buffer(buffer, count, buffer_index);
+		buffer[(*buffer_index)++] = hex_str[u];
 	}
 }
