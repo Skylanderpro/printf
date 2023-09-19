9a2192ccf2ad2614459aa838c370ad813e5fb370
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-19 07:29:36 +0300

    Applied modifications to files: main.h

--- main.h
+++ main.h
@@ -15,11 +15,16 @@ void print_c(char c, char *buffer, int *count, int *buffer_index);
 void print_s(const char *str, char *buffer, int *count, int *buffer_index);
 void print_di(int num, char *buffer, int *count, int *buffer_index);
 void print_perc(char *buffer, int *count, int *buffer_index);
-void print_binary(unsigned int num, char *buffer, int *count, int *buffer_index);
-void print_unsigned_int(unsigned int, char *buffer, int *count, int *buffer_index);
+void print_binary(unsigned int num, char *buffer, int *count,
+		int *buffer_index);
+void print_unsigned_int(unsigned int, char *buffer, int *count,
+		int *buffer_index);
 void print_octal(unsigned int, char *buffer, int *count, int *buffer_index);
-void print_hexad_lower(unsigned int, char *buffer, int *count, int *buffer_index);
-void print_hexad_upper(unsigned int, char *buffer, int *count, int *buffer_index);
-void handle_format(const char *ptr, va_list vargs, char *buffer, int *count, int *buffer_index, unsigned int *num);
+void print_hexad_lower(unsigned int, char *buffer, int *count,
+		int *buffer_index);
+void print_hexad_upper(unsigned int, char *buffer, int *count,
+		int *buffer_index);
+void handle_format(const char *ptr, va_list vargs, char *buffer, int *count,
+		int *buffer_index, unsigned int *num);
 
 #endif
